c1c59d6eb969dd157e1076481e29b3e555c8fc59
Denny Sander <sander.bewerbung@gmx.de>
2019-03-04 12:18:49 +0100

    Add Edit Action with Tests

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(chefname: "DennyChef", email:"chefdenny@chef.com",
+                        password: "password", password_confirmation: "password")
+  end
+
+  test 'reject an invalid edit' do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: {chefname:" ", email: "denny@chef.com"} }
+    assert_template 'chefs/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test 'accept valid signup' do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    post chef_path(@chef), params: {chef: { chefname:"denny1", email: "denny1@test.de" } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "denny1", @chef.chefname
+    assert_match "denny1@test.de", @chef.email
+  end
+
+end
