b0a68c3ba0df22fe2df76cf70e3b0cd05282f64f
Denny Sander <sander.bewerbung@gmx.de>
2019-04-17 22:25:05 +0200

    Complete Comments Feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
 
-  resources :recipes
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
   # get '/recipes', to: 'recipes#index'
   # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
