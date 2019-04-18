c7838e6211834e5b22bad8e0444592f65110e557
Denny Sander <sander.bewerbung@gmx.de>
2019-04-18 11:40:11 +0200

    Make comments real-time

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
+  mount ActionCable.server => '/cable'
 end
